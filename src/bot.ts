abab0c0e4706c77f3734bbd93af87192493aea3f
Jaron <jaron_zijlstra@outlook.com>
2021-05-30 21:32:32 +0200

    Feat: using webhook for stats logging
    Remember to add STATS_WEBHOOK_ID and STATS_WEBHOOK_TOKEN to .env

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import { Client, DMChannel, MessageEmbed, NewsChannel, TextChannel } from "discord.js";
+import { Client, DMChannel, MessageEmbed, NewsChannel, TextChannel, WebhookClient } from "discord.js";
 
 dotenv.config();
 
@@ -12,46 +12,55 @@ const client = new Client();
 let channel: TextChannel | NewsChannel;
 
 const setStats = async (channel: TextChannel | NewsChannel) => {
+	const {STATS_WEBHOOK_ID, STATS_WEBHOOK_TOKEN} = process.env;
+	if(!STATS_WEBHOOK_ID || !STATS_WEBHOOK_TOKEN) return console.warn("Stats webhook config has not been set");
+	const webhook = new WebhookClient(STATS_WEBHOOK_ID, STATS_WEBHOOK_TOKEN);
 	const guild = channel.guild;
 	const members = await guild.members.fetch();
-	const sentMessage = await channel.send(new MessageEmbed({
-		color: "BLUE",
-		title: "Bot and server stats",
-		fields: [
-			{
-				name: "All members",
-				value: members.size
-			},
-			{
-				name: "Members",
-				value: members.filter(member => !member.user.bot).size
-			},
-			{
-				name: "Bots",
-				value: members.filter(member => member.user.bot).size
-			},
-			{
-				name: "Categories",
-				value: guild.channels.cache.filter(channel => channel.type === "category").size
-			},
-			{
-				name: "Channels",
-				value: guild.channels.cache.filter(channel => channel.type !== "category").size
-			},
-			{
-				name: "Roles",
-				value: guild.roles.cache.size
-			},
-			{
-				name: "Emojis",
-				value: guild.emojis.cache.filter(emoji => !emoji.animated).size
-			},
-			{
-				name: "Animated Emojis",
-				value: guild.emojis.cache.filter(emoji => emoji.animated).size
-			}
+	const sentMessage = await webhook.send({
+		avatarURL: client.user?.displayAvatarURL(),
+		username: "Pixel Pizza Stats",
+		embeds: [
+			new MessageEmbed({
+				color: "BLUE",
+				title: "Bot and server stats",
+				fields: [
+					{
+						name: "All members",
+						value: members.size
+					},
+					{
+						name: "Members",
+						value: members.filter(member => !member.user.bot).size
+					},
+					{
+						name: "Bots",
+						value: members.filter(member => member.user.bot).size
+					},
+					{
+						name: "Categories",
+						value: guild.channels.cache.filter(channel => channel.type === "category").size
+					},
+					{
+						name: "Channels",
+						value: guild.channels.cache.filter(channel => channel.type !== "category").size
+					},
+					{
+						name: "Roles",
+						value: guild.roles.cache.size
+					},
+					{
+						name: "Emojis",
+						value: guild.emojis.cache.filter(emoji => !emoji.animated).size
+					},
+					{
+						name: "Animated Emojis",
+						value: guild.emojis.cache.filter(emoji => emoji.animated).size
+					}
+				]
+			})
 		]
-	}));
+	});
 	for(const message of (await channel.messages.fetch({before: sentMessage.id})).values()){
 		if(message.deletable) await message.delete();
 	}
